5c7e0cf8ba36d768b3b98ff3daf35333a4202c8b
theajack <1506282385@qq.com>
2023-09-18 09:32:22 +0800

    feat: full-custom

--- examples/12-custom-renderer/03-full-custom:iframe.tsx
+++ examples/12-custom-renderer/03-full-custom:iframe.tsx
@@ -1,4 +1,4 @@
-import { defineRenderer } from 'alins';
+import { IElement, defineRenderer, ILifeListener } from 'alins';
 
 const ElementType = {
     Element: 0,
@@ -27,10 +27,16 @@ defineRenderer({
     isElement (node) {
         return node.type === ElementType.Element || node.type === ElementType.Text;
     },
+    onMounted (parent: LogElement, node: LogElement, mounted: ILifeListener<void|ILifeListener>) {
+        node.mountCallList.push(mounted);
+    },
+    onRemoved (parent: LogElement, node: LogElement, removed: ILifeListener) {
+        node.removeCallList.push(removed);
+    },
 });
 
-class LogElement {
-    static Root = null;
+class LogElement implements IElement {
+    static Root: null|LogElement = null;
     type = ElementType.Element;
     style = {}; // mock
     tagName = '';
@@ -57,21 +63,26 @@ class LogElement {
         this.innerText = text;
         if (tag === 'Root') LogElement.Root = this;
     }
-    parentElement = null;
+    parentElement: LogElement|null = null;
     get parentNode () {return this.parentElement;};
+    removeCallList: any[] = [];
     remove () {
         const children = this.parentElement?.children;
         if (children) {
             children.splice(children.indexOf(this), 1);
+            this.removeCallList.forEach(call => call(this));
         }
     }
     get innerHTML () {return this.innerText;}
-    children = [];
+    get outerHTML () {return this.innerText;}
+    children: LogElement[] = [];
     get childNodes () {
         return this.childNodes;
     }
-    appendChild (child) {
+    mountCallList: any[] = [];
+    appendChild (child: LogElement) {
         this.children.push(child);
+        child.mountCallList.forEach(call => call(child));
     }
     get nextSibling () {
         return this.parentElement?.children[this.index + 1] || null;
@@ -81,13 +92,13 @@ class LogElement {
             throw new Error('insertBefore error');
         }
         this.parentElement?.children.splice(child.index - 1, 0, node);
+        child.mountCallList.forEach(call => call(child));
         return node;
     }
     get index () {
         const parent = this.parentElement;
         return !parent ? -1 : parent.children.indexOf(this);
     }
-
     render () {
         const text = `${this.innerText}`;
         if (this.type === ElementType.Text) {
